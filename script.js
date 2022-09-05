eb0ce0fb0d687096d7f7627c2b15c6580a954e84
Sebastian Alvarez <sebastiangit60@gmail.com>
2022-09-05 15:47:34 -0400

    reorganizando el codigo y aniadiendo funcionalidades

--- script.js
+++ script.js
@@ -1,9 +1,50 @@
 const container = document.querySelector('.container')
 const text = document.getElementById('text');
 
+class Operaciones {
+
+    constructor(valor){
+       this.valor = valor;
+    }
+
+    eliminarUltimoDigito() {
+        let textoEliminado = this.valor.value.split('')
+        this.valor.value = ''
+        textoEliminado.pop()
+        textoEliminado.map(elementico => {
+            this.valor.value += elementico
+        })
+    }
+    sacarRaizCuadrada() {
+        let raiz_cuadrada = Math.sqrt(this.valor.value)
+        if (isNaN(raiz_cuadrada)) {
+            this.valor.value = ''
+            return alert('operacion invalida')
+        }
+        return this.valor.value = raiz_cuadrada
+    }
+    elevarAlCuadrado() {
+        let laPotencia = parseFloat(this.valor.value * this.valor.value)
+        if(isNaN(laPotencia)){
+            this.valor.value = ''
+            return alert('invalid Acction')
+        }
+        return this.valor.value = laPotencia
+    }
+    elevarUnNumeroAotro() {
+        let desfragmentar = this.valor.value.split('^')
+        let laPotencia = Math.pow(desfragmentar[0], desfragmentar[1])
+        if (!isNaN(laPotencia)) {
+            return this.valor.value = laPotencia
+        }
+        return alert('invalid Acction')
+    }
+
+}
 container.childNodes.forEach((element) => {
     element.childNodes.forEach(elemento => {
         elemento.addEventListener('click', () => {
+            const operacion = new Operaciones(text)
             switch (elemento.value) {
                 case 'CE':
                     text.value = "";
@@ -13,12 +54,7 @@ container.childNodes.forEach((element) => {
                     text.value = "";
                     break;
                 case 'D':
-                    let textoEliminado = text.value.split('')
-                    text.value = ''
-                    textoEliminado.pop()
-                    textoEliminado.map(elementico => {
-                        text.value += elementico
-                    })
+                    operacion.eliminarUltimoDigito()
                     break;
                 case 'X':
                     text.value += '*'
@@ -34,6 +70,17 @@ container.childNodes.forEach((element) => {
                     text.value += '-'
                     break;
 
+                case 'sqrt':
+                    operacion.sacarRaizCuadrada()
+                    break;
+
+                case 'x2':
+                    operacion.elevarAlCuadrado()
+                    break;
+
+                case '^':
+                    operacion.elevarUnNumeroAotro()
+
                 default:
                     text.value += elemento.value
             }
@@ -42,3 +89,6 @@ container.childNodes.forEach((element) => {
     })
 })
 
+text.addEventListener('click', () => {
+    text.value = ''
+})
